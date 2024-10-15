29d7787456e9985b19f8faeefcff4773910d57bd
monibbormon <monibbormon14@gmail.com>
2024-10-15 10:42:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import { connect } from "mongoose";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express();
+const port = "https://tasty-dash-backend.vercel.app" || 4000;
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://tasty-dash.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+// database connection
+connectDB();
+
+// api endpoint
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads")); // we can access the image at images/image_name now
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("api working");
+});
+
+app.listen(port, () => {
+  console.log(`server started on http://localhost:${port}`);
+});
+
+export default app;
